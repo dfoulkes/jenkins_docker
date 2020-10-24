44c57afbd908a155e54ca18b61ee23af5137c2df
dfoulkes <danfoulkes@gmail.com>
2020-10-24 12:15:55 +0100

    improve the install script

--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 #install docker compose
-echo "installing docker compose"
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-source ~/.profile
+
+if ! command -v COMMAND &> /dev/null
+then
+    echo "installing docker compose"
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+    sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+    source ~/.profile
+fi
 
 if [ ! -d "./jenkins_dir/casc_configs" ] 
 then
